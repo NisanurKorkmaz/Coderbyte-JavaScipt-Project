2a8a1732398381f3c5b3a0c447d50aa95b0f4d6b
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-13 10:06:19 +0300

    Update 2-PalindromeTwo.js

--- Medium-Projects/2-PalindromeTwo.js
+++ Medium-Projects/2-PalindromeTwo.js
@@ -1,6 +1,6 @@
 function PalindromeTwo(str){
-
-
+    
+    alfabets=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","v","x","y","z"]
 
     str= str.toLowerCase();
 
