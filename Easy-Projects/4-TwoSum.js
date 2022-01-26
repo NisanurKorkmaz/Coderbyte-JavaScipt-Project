d7c5fdd61886bbd3da391d15402eb4e186101640
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-26 16:22:59 +0300

    Add files via upload

---
+++ Easy-Projects/4-TwoSum.js
@@ -0,0 +1,35 @@
+function TwoSum(arr) { 
+
+  let first = arr[0] ;
+  let l= arr.length ;
+  let couple = [] ;
+  let t ;
+  let flag=0 ;
+
+  for(i=1 ; i<l ; i++){
+    for(j=i+1 ; j<l ; j++){
+      t=arr[i]+arr[j]
+      if(t==first){
+        couple.push(arr[i])
+        couple.push(arr[j])
+        flag=1
+      }
+
+    }
+
+
+  }
+  if(flag=0){
+    return -1
+  }else{
+    let c="";
+    for(a=0 ; a<couple.length ; a=a+2){
+      c=c+couple[a]+","+couple[a+1]+" "
+
+    }
+    return c ;
+  }
+}
+   
+// keep this function call here 
+console.log(TwoSum(readline()));
