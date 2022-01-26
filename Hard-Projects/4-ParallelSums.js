c446a233e2ef809a3dfdee069f1239a9de63d769
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-26 16:23:40 +0300

    Add files via upload

---
+++ Hard-Projects/4-ParallelSums.js
@@ -0,0 +1,56 @@
+function ParallelSums(arr) { 
+
+  let a1 = [];
+  let a2 = [];
+  let toplam=0;
+
+  for(a=0 ; a<arr.length ; a++){
+    toplam=toplam+arr[a]
+  }
+
+  let yarisi=toplam/2;
+  //console.log(yarisi);
+
+  for(i=0 ; i<arr.length ; i=i+2){
+    a1.push(arr[i]);
+    a2.push(arr[i+1]);
+  }
+  let t1=0 ;
+  for(b=0 ; b<arr.length ; b++){
+    for(c=0 ; c<a1.length ; c++){
+        t1=t1+a1[c]
+    }
+    //console.log("t1 :"+t1)
+    if(t1==yarisi){
+      //console.log(a1,a2)
+      let a="";
+      for(k=0 ; k<a1.length ; k++){
+        a=a+a1[k]+","
+      }
+      for(l=0 ; l<a2.length ; l++){
+        if(l==(a2.length-1)){
+          a=a+a2[l]
+        }else{
+          a=a+a2[l]+","
+        }
+      }
+      
+      return a
+    }else{
+      //iki array arasi eleman degisimi 
+      let temp;
+      temp=a1[b]
+      a1[b]=a2[b]
+      a2[b]=temp
+    }
+    t1=0
+  }
+
+
+
+  return -1; 
+
+}
+   
+// keep this function call here 
+console.log(ParallelSums(readline()));
