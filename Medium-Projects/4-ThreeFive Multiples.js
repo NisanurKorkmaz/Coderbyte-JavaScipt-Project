02587fcec6f821fe7c4ec275444621674df18e50
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-26 16:24:25 +0300

    Update 4-ThreeFive Multiples.js

--- Medium-Projects/4-ThreeFive Multiples.js
+++ Medium-Projects/4-ThreeFive Multiples.js
@@ -3,17 +3,16 @@ function ThreeFiveMultiples(num) {
   let toplam = 0 ;
   for(i=0 ; i<num ; i++){
     if(i%3==0){
-      //console.log(i)
+      
       toplam=toplam+i;
     }
     else if(i%5==0){
       toplam=toplam+i
-      //console.log(i)
+      
     }
   } 
   return toplam; 
 
 }
-   
-// keep this function call here 
+ 
 console.log(ThreeFiveMultiples(readline()));
