9df0eeee742e52747e20ad79c8ef4ad693aefe0b
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-02-04 19:45:55 +0300

    Add files via upload

---
+++ Easy-Projects/5-PowerSetCount.js
@@ -0,0 +1,10 @@
+function PowerSetCount(arr) { 
+  let l = arr.length
+  let result = Math.pow(2,l)
+
+  return result; 
+
+}
+   
+// keep this function call here 
+console.log(PowerSetCount(readline()));
